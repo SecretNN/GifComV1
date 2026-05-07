9a1a679de0950f7a4403d011dbb2fbe843e6cc85
secret <secretkto587@gmail.com>
2026-05-08 00:43:42 +0300

    Початок роботи на сейвом гіф

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 import flask
-from flask import render_template, Flask
+from flask import render_template, Flask, request, redirect
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask (__name__)
@@ -10,7 +10,7 @@ db = SQLAlchemy(app)
 class Gifs1(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(70), nullable=False)
-    text = db.Column(db.Text, nullable=False)
+    Gifs = db.Column(db.String(255), nullable=False)
 
 @app.route('/main')
 @app.route('/')
@@ -25,9 +25,23 @@ def profile():
 def mygif():
     return render_template('mygif.html')
 
-@app.route('/sendgif')
+@app.route('/sendgif', methods=['POST', 'GET'])
 def sendgif():
-    return render_template('sendgif.html')
+    if request.method == 'POST':
+        title = request.form['title']
+        Gifs = request.form['Gifs']
+
+        post = Gifs1(title=title, Gifs=Gifs)
+
+        try:
+            db.session.add(post)
+            db.session.commit()
+            return redirect('/')
+        except:
+            return 'Помилка'
+
+    else:
+        return render_template('sendgif.html')
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0', port=5000)
